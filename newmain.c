e249f9056f4a418f15d1af569adf93fca092d4ab
srudhy <srudhy@gmail.com>
2013-10-03 08:48:23 -0700

    Second change 2

---
+++ newmain.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   newmain.c
+ * Author: mathew
+ *
+ * Created on October 3, 2013, 8:44 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
